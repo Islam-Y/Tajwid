cace3993dac054f8bda565779e5a594c36d9b89b
Islam-Y <yunusoffislam@yandex.ru>
2026-02-15 18:26:18 +0300

    initial commit

---
+++ src/main/java/ru/muslim/tajwid/TajwidApplication.java
@@ -0,0 +1,13 @@
+package ru.muslim.tajwid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajwidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TajwidApplication.class, args);
+	}
+
+}
